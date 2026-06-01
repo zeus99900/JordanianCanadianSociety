28145af8dfc5e0c8c8ef8cbe78d8874576a05dbc
Nashama Admin <admin@jordaniancanadian.ca>
2026-06-01 01:55:08 -0300

    Fix Stripe API version mismatch for live account

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,3 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-08-27.basil',
-  typescript: true,
-});
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
